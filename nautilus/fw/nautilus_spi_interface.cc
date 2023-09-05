f4ded7fc5f89335c0e29bbf5a165708de089d5bc
Matthieu Vigne <matthieu.vigne@laposte.net>
2023-09-05 20:14:44 +0200

    Start filling SPI read function

--- nautilus/fw/nautilus_spi_interface.cc
+++ nautilus/fw/nautilus_spi_interface.cc
@@ -1,10 +1,9 @@
 // Copyright 2023 Matthieu Vigne
-
 #include "nautilus_spi_interface.h"
 #include "fw/stm32g4_async_uart.h"
 #include "PeripheralPins.h"
 
-
+#define M_PI       3.14159265358979323846f
 
 using namespace nautilus;
 
@@ -35,6 +34,18 @@ USART_TypeDef* debug_uart_ = nullptr;
 uint16_t timeCnt = 0;
 
 
+
+
+/*static DigitalOut led1_(PF_0, 1);
+static bool isOn = true;
+
+isOn = !isOn;
+if (isOn)
+    led1_ = 1;
+else
+    led1_ = 0;
+*/
+
 NautilusSPIInterface::NautilusSPIInterface(moteus::BldcServo* bldcIn,
                                            moteus::BldcServo::CommandData* commandIn)
 {
@@ -44,23 +55,55 @@ NautilusSPIInterface::NautilusSPIInterface(moteus::BldcServo* bldcIn,
 
 void NautilusSPIInterface::poll()
 {
-
-    debug_uart_->TDR = (SPICR1 & 0xFF);
-
-    //for (int i = 0; i < posInMessage; i++)
-    //    debug_uart_->TDR = rxBuffer[i];
-
     timeCnt++;
-
 }
 
+uint32_t fToUInt(float const& f)
+{
+    union result{
+        float f;
+        uint32_t i;
+    };
+    union result res;
+    res.f = f;
+    return res.i;
+}
 
 uint32_t nautilus::processReadCommand(uint8_t const& registerAddress)
 {
+
     switch ((registerAddress))
     {
-    case SPIRegister::currentMode: return 1;
-    case SPIRegister::faultCode: return 55;
+    case SPIRegister::currentMode: return bldc->status().mode;
+    case SPIRegister::faultCode: return static_cast<uint32_t>(bldc->status().fault);
+    case SPIRegister::measuredPosition: return fToUInt(static_cast<float>(bldc->motor_position().sources[0].filtered_value / bldc->motor_position_config()->sources[0].cpr * 2 * M_PI));
+    case SPIRegister::measuredVelocity: return bldc->motor_position().sources[0].raw;
+    case SPIRegister::measuredIQ: return bldc->motor_position().sources[0].nonce;
+    case SPIRegister::measuredIPhaseA: return 0;
+    case SPIRegister::measuredIPhaseB: return 0;
+    case SPIRegister::measuredIPhaseC: return 0;
+    case SPIRegister::measuredUPhaseA: return 0;
+    case SPIRegister::measuredUPhaseB: return 0;
+    case SPIRegister::measuredUPhaseC: return 0;
+    case SPIRegister::measuredMotTemp: return 0;
+    case SPIRegister::measuredDriveTemp: return 0;
+    case SPIRegister::measuredUBat: return 0;
+    case SPIRegister::targetPosition: return 0;
+    case SPIRegister::targetVelocity: return 0;
+    case SPIRegister::targetIQ: return 0;
+    case SPIRegister::rawEncoderPos: return 0;
+    case SPIRegister::encoderOrientation: return 0;
+    case SPIRegister::commutationOffset: return 0;
+    case SPIRegister::currentLoopKp: return 0;
+    case SPIRegister::currentLoopKI: return 0;
+    case SPIRegister::currentLoopIntMax: return 0;
+    case SPIRegister::velocityLoopKp: return 0;
+    case SPIRegister::velocityLoopKI: return 0;
+    case SPIRegister::velocityLoopIntMax: return 0;
+    case SPIRegister::motorMaxCurrent: return 0;
+    case SPIRegister::motorMaxTemperature: return 0;
+    case SPIRegister::driverMaxTemperature: return 0;
+    case SPIRegister::commTimeout: return 0;
     default: return 0;
     }
 }
@@ -84,28 +127,18 @@ void SPI3_IRQHandler(void)
 
     if (posInMessage == 3)
     {
-
-
-        static DigitalOut led1_(PF_0, 1);
-        static bool isOn = true;
-
         // We have received enough info from the user to know what to do.
         uint32_t data = 0;
         if (rxBuffer[0] == static_cast<uint8_t>(SPICommand::regRead))
         {
             // Handle read request
             data = nautilus::processReadCommand(rxBuffer[1]);
-            isOn = !isOn;
-            if (isOn)
-                led1_ = 1;
-            else
-                led1_ = 0;
         }
 
-        // We have received enough info from the user to send the next 4 bytes
-        txBuffer[3] = (data << 24) & 0xFF;
-        txBuffer[4] = (data << 16) & 0xFF;
-        txBuffer[5] = (data << 8) & 0xFF;
+        // Send next 4 bytes.
+        txBuffer[3] = (data >> 24) & 0xFF;
+        txBuffer[4] = (data >> 16) & 0xFF;
+        txBuffer[5] = (data >> 8) & 0xFF;
         txBuffer[6] = (data) & 0xFF;
         * ((__IO uint8_t *) & SPI3->DR) = (uint8_t) txBuffer[3];
         * ((__IO uint8_t *) & SPI3->DR) = (uint8_t) txBuffer[4];
@@ -148,7 +181,7 @@ void EXTI15_10_IRQHandler(void)
         // Third byte: status
         txBuffer[0] = (timeCnt >> 8) & 0xFF;
         txBuffer[1] = timeCnt & 0xFF;
-        txBuffer[2] = 0x42;
+        txBuffer[2] = bldc->status().mode;
 
         * ((__IO uint8_t *) & SPI3->DR) = (uint8_t) txBuffer[0];
         * ((__IO uint8_t *) & SPI3->DR) = (uint8_t) txBuffer[1];
@@ -162,14 +195,9 @@ void EXTI15_10_IRQHandler(void)
 void NautilusSPIInterface::setup()
 {
     // Clear oscillator config
-    // RCC->CR &= ~(1 << 16);
-    // RCC->BDCR &= ~(1 << 0);
-
-
     const auto uart = pinmap_peripheral(
         PC_10_ALT0, PinMap_UART_TX);
     debug_uart_ = reinterpret_cast<USART_TypeDef*>(uart);
-
 
     // led1_ = 0;
     spi_init(&spi_,
@@ -183,8 +211,6 @@ void NautilusSPIInterface::setup()
     // SPI config: see https://www.youtube.com/watch?v=_RBXQLPGr7Q
     // Enable interrupt on SPI3 RX
     NVIC->ISER[1] |= (1u << 19);
-
-
 
     // Interrupt on slave-select edges.
 
